40ce6bafb0b42ec7772c4b6b50656f8b2de15476
Paul <phahn425@gmail.com>
2016-10-19 16:54:34 -0400

    pj1 and pj2

---
+++ pj1/goldbach2.c
@@ -0,0 +1,185 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define BITSPERSEG (8*256*sizeof(int))
+
+typedef struct seg
+{
+
+	int bits[256];
+	struct seg *next,*prev;
+
+}seg;
+seg *head, *pt;
+//whichseg:(j-3)/16384
+//whichint: ((j-3)%16384)/64
+//whichbit: ((j-3)/2)%32
+int whichint(int j)
+{
+return(((j-3)%16384)/64);
+
+}
+
+int whichbit(int j)
+{
+
+int x = ((j-3)/2)%32;
+return x;
+}
+
+seg* whichseg(int j)
+{
+
+static seg *lastseg;
+static int lastj=0;
+if(lastseg==NULL)lastseg = head;
+seg *x = head;
+int y = (j-3)/16384;
+if(j>lastj) 
+{
+y= y-((lastj-3)/16384);
+x= lastseg;
+}		
+for(int i=0;i<y;i++)
+{
+x = x->next;
+
+}
+lastseg = x;
+lastj=j;
+
+return x;
+}
+
+int testbit(int j,int q, seg *k)
+{
+
+int x = j;
+int y = q;
+seg *z = k;
+return( (z->bits[x]&(1<<y))==0);
+}
+
+int marknoprime (int j)
+{
+int x = whichint(j);
+int y = whichbit(j);
+seg *z = whichseg(j);
+
+if(testbit(x,y,z))
+{
+z->bits[x] |= 1<< y;
+}
+
+}
+
+void main (int argc, char *argv[])
+{
+int input;
+int number;
+if(argc==2) sscanf(argv[1],"%d",&input);
+	else return;
+number = input;
+input = (input+((BITSPERSEG)-1))/(2*BITSPERSEG);
+
+head = (seg*)malloc(sizeof(seg));
+pt = head;
+
+seg* temp = (seg*)malloc(sizeof(seg));
+
+for(int i=0;i<input;i++)
+{
+temp = pt;
+pt->next = (seg*)malloc(sizeof(seg));
+pt = pt->next;
+pt->prev = temp;
+}
+//===================================================================Memory Allocated
+for(int i=3;i<=number;i=i+2)
+{
+	for(int j=2;i*j<=number;j++)
+	{
+
+		if(((i*j)%2)!=0)
+		{		
+		marknoprime(i*j);
+		}
+	}	
+		
+		
+}
+//The bit representing non-prime odd number has been marked 
+
+int count=0;
+for(int i=3;i<=number;i=i+2)
+	{
+	
+	int x=whichint(i);
+	int y=whichbit(i);
+	seg*z=whichseg(i);
+	if(testbit(x,y,z)) 
+		{ 	  
+   		  count++;
+		}
+	}
+printf("Calculating odd primes up to %d\n",number);
+printf("Found %d odd primes \n",count);
+//Counted and checked all the odd prime numbers. 
+//===============================================================================================
+//goldbach begins
+int goldNUM;
+printf("Enter Even Numbers >5 for Goldbach Tests: ");
+
+while( scanf("%d",&goldNUM) != EOF )
+{
+if((goldNUM%2)!=0)
+{
+printf("Please enter an even number \n");
+continue;
+}
+int bgnINT=whichint(3);
+int bgnBIT=whichbit(3);
+seg*bgnSEG=whichseg(3);
+
+int endINT=whichint(goldNUM-3);
+int endBIT=whichbit(goldNUM-3);
+seg*endSEG=whichseg(goldNUM-3);
+int counter=0;
+int biggy=0;
+for(int i=3;i<=(goldNUM/2);i=i+2)
+	{
+		if(testbit(bgnINT,bgnBIT,bgnSEG)&testbit(endINT,endBIT,endSEG))
+		{
+			if(i>biggy) biggy=i;
+			counter++;
+		}
+		bgnBIT++;
+		endBIT--;
+		if(bgnBIT>31)
+			{
+			bgnBIT=0;
+			bgnINT++;
+			}
+
+		if(endBIT<0)	
+			{
+			endBIT=31;
+			endINT--;
+			}	
+	
+		if(bgnINT>255)
+			{
+			bgnINT=0;
+			bgnSEG = bgnSEG->next;			
+			}
+		if(endINT<0)
+			{
+			endINT=255;
+			endSEG = endSEG->prev;
+			}
+	}
+
+
+printf("The Largest %d = %d + %d out of %d solutions\n",goldNUM,biggy,goldNUM-biggy,counter);
+}
+}
---
+++ pj2_proto/myar.c
@@ -0,0 +1,222 @@
+#include <dirent.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <string.h>
+#include <fcntl.h>
+#include <time.h>
+#include <sys/cdefs.h>
+#include <ar.h>
+#include <string.h>
+#include <time.h>
+#define ARMAG	"!<arch>\n"	/* String that begins an archive file.  */
+#define SARMAG	8		/* Size of that string.  */
+#define ARFMAG	"`\n"		/* String in ar_fmag at end of each header.  */
+struct dirent mydirect;
+struct ar_hdr myheader;
+struct stat archivestat;
+struct stat filestat;
+/* for git diff test */
+/* for git(this is for test word diff) diff head*/
+void t_function(char archivename[])
+{
+int archivefd= open(archivename,0);
+fstat(archivefd, &archivestat);
+lseek(archivefd,8,SEEK_SET);
+int filesize=0;
+int archivesize=archivestat.st_size-8;
+while(425)
+	{
+	char* filename=(char*)malloc(sizeof(myheader.ar_name));	
+	struct ar_hdr myheader;
+	read(archivefd, &myheader, 60);
+	archivesize = archivesize-60;	
+	sscanf(myheader.ar_size,"%d",&filesize);
+	if(filesize%2!=0) filesize = filesize+1;	
+	archivesize=archivesize-filesize;
+	sscanf(myheader.ar_name,"%s",filename);
+	int i=0;	
+	
+	for(i=0;i<sizeof(filename);i++)
+	{
+		if(filename[i]=='/') filename[i]=' ';
+	}
+	printf("%s\n",filename);	
+	lseek(archivefd,filesize,SEEK_CUR);
+	if(archivesize==0) return;	
+	}
+}
+
+void x_function(char archivename[], char file[])
+{
+int fd;
+int archivefd= open(archivename,0);
+int filefd   = open(file   ,0);
+
+fstat(archivefd, &archivestat);
+fstat(filefd   , &filestat   );
+lseek(archivefd,8,SEEK_SET);
+int filesize=0;
+int archivesize=archivestat.st_size-8;
+char* filebuf;
+int count = strlen(file);
+while(425)
+	{	
+	char* filename = (char*)malloc(sizeof(myheader.ar_name));
+	struct ar_hdr myheader;
+	read(archivefd, &myheader, 60);
+	archivesize = archivesize-60;
+	sscanf(myheader.ar_size,"%d",&filesize);
+	if(filesize%2!=0) filesize = filesize+1;
+	sscanf(myheader.ar_name,"%s",filename);	
+	int i=0;
+	for(i=0;i<sizeof(filename);i++)
+	{
+		if(filename[i]=='/') filename[i]=' ';
+	}
+		
+	if(strncmp(filename,file,count)==0)
+		{
+		printf("%d",filesize);
+		filebuf = (char*)malloc(filesize);
+		read(archivefd,filebuf,filesize);
+		break;		
+		}
+	lseek(archivefd,filesize,SEEK_CUR); 
+	archivesize = archivesize-filesize;	
+	if(archivesize<0) 
+		{
+		printf("Wrong input, file does not exist in archive\n");
+		return;	
+		}	
+	}
+int newfd = creat(file,0666);
+write(newfd, filebuf,filesize);
+}
+
+void v_function(char archivename[])
+{
+int archivefd = open(archivename,0);
+fstat(archivefd,&archivestat);
+lseek(archivefd,8,SEEK_SET);
+int filesize = 0;
+int archivesize = archivestat.st_size-8;
+long mode;
+while(425)
+	{
+	char* filename=(char*)malloc(sizeof(myheader.ar_name));	
+	char* filemode=(char*)malloc(sizeof(myheader.ar_mode));
+	char* timestam=(char*)malloc(sizeof(myheader.ar_date));
+	char* fileuid =(char*)malloc(sizeof(myheader.ar_uid ));
+	char* filegid =(char*)malloc(sizeof(myheader.ar_gid ));
+	char* cfilesize=(char*)malloc(sizeof(myheader.ar_size));
+	struct ar_hdr myheader;
+	read(archivefd, &myheader, 60);
+	archivesize = archivesize-60;	
+	sscanf(myheader.ar_size,"%d",&filesize);
+	if(filesize%2!=0) filesize = filesize+1;	
+	archivesize=archivesize-filesize;
+	sscanf(myheader.ar_name,"%s",filename);
+	int i=0;	
+	
+	for(i=0;i<sizeof(filename);i++)
+	{
+		if(filename[i]=='/') filename[i]=' ';
+	}
+	sscanf(myheader.ar_mode,"%s",filemode);
+	sscanf(ctime(&filestat.st_mtime),"%s",timestam);
+	sscanf(myheader.ar_uid ,"%s",fileuid);
+	sscanf(myheader.ar_gid ,"%s",filegid);
+	sscanf(myheader.ar_size,"%s",cfilesize);
+	lseek(archivefd,filesize,SEEK_CUR);
+	printf("%s %s/%s %s %s %s\n", filemode, fileuid,filegid,timestam, cfilesize,filename); 
+	if(archivesize==0) return;	
+	}
+}
+
+
+void q_function(char archivename[], char filename[])
+{
+if(open(filename,0)==-1)
+	{
+
+	printf("Wrong file name\n");
+	return;
+
+	}
+int filefd   = open(filename,0666);
+fstat(filefd,&filestat);
+char* buffer = (char*) malloc(filestat.st_blksize);
+char* headbuffer = (char*)malloc(60);
+read(filefd,&buffer,filestat.st_size);
+strcat(filename,"/");
+sprintf(headbuffer,"%-16s%-12ld%-6d%-6d%-8d%-10ld%-2s",filename,filestat.st_mtime,filestat.st_uid,filestat.st_gid,filestat.st_mode,filestat.st_size,ARFMAG);
+int i=0;
+while(headbuffer[i]!=' ')i++;
+headbuffer[i]=='/';
+int fdtemp = open(archivename,000);
+if(fdtemp==-1)
+{
+close(fdtemp);
+int newfd = creat(archivename,0666);
+write(newfd,ARMAG,SARMAG);
+write(newfd,headbuffer,60);
+write(newfd,&buffer,filestat.st_size);
+}
+
+else
+{
+int archivefd= open(archivename,0666);
+fstat(archivefd, &archivestat);
+lseek(archivefd,0,SEEK_END);
+write(archivefd,headbuffer,60);
+write(archivefd,&buffer,filestat.st_size);
+}
+
+}
+
+void a_function(char archive[])
+{  
+
+DIR *dp;
+struct dirent *ep;
+char dirfilename[15];
+  
+dp = opendir ("./");
+int i=0;
+if (dp != NULL)
+    {
+      while (ep = readdir (dp))
+	{
+        
+		if(ep->d_name[0]!='.')
+	   {        
+		sprintf(dirfilename,"%s",(ep->d_name));
+ 		   if(strcmp(archive,dirfilename)!=0)
+		   {	
+		   stat(dirfilename,&filestat);	
+		   long tpassed;
+        	   time(&tpassed);
+		   long tdiff = tpassed - filestat.st_mtime;
+	           printf("%s:%ld\n",dirfilename,tdiff);
+		   if(1<=(tdiff/3600)) q_function(archive,dirfilename);	
+		   }
+	   }
+	}	
+      (void) closedir (dp);
+    
+	}
+
+}
+
+void main(int argc, char* argv[])
+{
+//t_function(argv[1]);
+x_function(argv[1],argv[2]);
+//v_function(argv[1]);
+//q_function(argv[1], argv[2]);
+//a_function(argv[1]);
+
+}	
